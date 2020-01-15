ddcf6708128cb92678363631b3d0ffbedabab402
seandolinar <sjjd199@gmail.com>
2020-01-15 13:01:14 -0500

    form subscribe examples

--- src/components/FormSubscribe.js
+++ src/components/FormSubscribe.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 import addToMailchimp from 'gatsby-plugin-mailchimp'
@@ -31,48 +31,64 @@ const FormSubscribe = () => {
         // });
 
         addToMailchimp(email, {}) // listFields are optional if you are only capturing the email address.
-        .then(data => {
-          // I recommend setting data to React state
-          // but you can do whatever you want (including ignoring this `then()` altogether)
-          console.log(data)
-        })
-        .catch(() => {
-          // unnecessary because Mailchimp only ever
-          // returns a 200 status code
-          // see below for how to handle errors
-        })
+            .then(data => {
+                // I recommend setting data to React state
+                // but you can do whatever you want (including ignoring this `then()` altogether)
+                console.log(data)
+                if (data.result === 'success') {
+                    setFormStatus('SUCCESS');
+                }
+            })
+            .catch(() => {
+                // unnecessary because Mailchimp only ever
+                // returns a 200 status code
+                // see below for how to handle errors
+            })
 
     }
 
 
     return <form className="form-subscribe" id="form-subscribe">
-        <div className="form-subscribe__field__email">
-            <label>Email</label>
-            <input 
-                id="form-subscribe__email"
-                type="email" 
-                required 
-            />
+        {formStatus !== 'SUCCESS' ? <><div className="form-subscribe__copy">
+            Get our latest blog posts straight to your inbox!
         </div>
-        {/* HONEY POT */}
-        <div 
-            style={{position: 'absolute',
-             left: '-5000px'}}
-            aria-hidden="true">
-                <input 
-                    id="form-subscribe__phone" 
-                    type="text" 
-                    name="b_9000c9c71a9eb1030fd6c4320_191d4d44cb" 
-                    tabindex="-1" 
-                    value="" 
-        />
-        </div>
-        <button
-            id="contact-form-submit"
-            className="fill-in-effect-orange"
-            type="submit"
-            onClick={onFormSubmission}
-        >SUBMIT</button>
+        <div className="form-subscribe__fields">
+            {/* <div className="form-subscribe__field__email"> */}
+                <label>Email: </label>
+                <input
+                    id="form-subscribe__email"
+                    type="email"
+                    required
+                />
+            {/* </div> */}
+            {/* HONEY POT */}
+            <div
+                style={{
+                    position: 'absolute',
+                    left: '-5000px'
+                }}
+                aria-hidden="true">
+                <input
+                    id="form-subscribe__phone"
+                    type="text"
+                    name="b_9000c9c71a9eb1030fd6c4320_191d4d44cb"
+                    tabindex="-1"
+                    value=""
+                />
+            </div>
+            <button
+                id="contact-form-submit"
+                className="fill-in-effect-orange"
+                type="submit"
+                onClick={onFormSubmission}
+            >SUBSCRIBE</button>
+        </div></> :
+        <>
+            <div className="form-subscribe__copy">
+                Thank you for subscribing!
+            </div>
+        </>
+    }
     </form>
 
 };
