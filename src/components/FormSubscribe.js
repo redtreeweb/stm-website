489855da4cd355990bd1ab2ff5a44ea3c7d1b741
seandolinar <sjjd199@gmail.com>
2020-01-16 14:06:09 -0500

    responsive fix

--- src/components/FormSubscribe.js
+++ src/components/FormSubscribe.js
@@ -61,53 +61,55 @@ const FormSubscribe = () => {
 
     return <div className="form-subscribe__wrapper">
         <form
-        className={`form-subscribe ${formStatus === 'FAIL' ? 'failed' : ''}`}
-        id="form-subscribe">
-        {formStatus !== 'SUCCESS' ? <><div className="form-subscribe__copy">
-            {
-                formStatus === 'FAIL' || formStatus === 'RESET' ?
-                    'Something went wrong. Please submit a valid email.' :
-                    'Get our latest blog posts straight to your inbox!'
-            }
-        </div>
-            <div className="form-subscribe__fields">
-                {/* <div className="form-subscribe__field__email"> */}
-                <label>Email: </label>
-                <input
-                    id="form-subscribe__email"
-                    type="email"
-                    required
-                />
-                {/* </div> */}
-                {/* HONEY POT */}
-                <div
-                    style={{
-                        position: 'absolute',
-                        left: '-5000px'
-                    }}
-                    aria-hidden="true">
-                    <input
-                        id="form-subscribe__phone"
-                        type="text"
-                        name="b_9000c9c71a9eb1030fd6c4320_191d4d44cb"
-                        tabindex="-1"
-                        value=""
-                    />
-                </div>
-                <button
-                    id="contact-form-submit"
-                    className="fill-in-effect-orange"
-                    type="submit"
-                    onClick={onFormSubmission}
-                >SUBSCRIBE</button>
-            </div></> :
-            <>
-                <div className="form-subscribe__copy">
-                    Thank you for subscribing!
+            className={`form-subscribe ${formStatus === 'FAIL' ? 'failed' : ''}`}
+            id="form-subscribe">
+            {formStatus !== 'SUCCESS' ? <><div className="form-subscribe__copy">
+                {
+                    formStatus === 'FAIL' || formStatus === 'RESET' ?
+                        'Something went wrong. Please submit a valid email.' :
+                        'Get our latest blog posts straight to your inbox!'
+                }
             </div>
-            </>
-        }
-    </form>
+                <div className="form-subscribe__fields">
+                    {/* <div className="form-subscribe__field__email"> */}
+                    <label>Email: </label>
+                    <input
+                        id="form-subscribe__email"
+                        type="email"
+                        required
+                    />
+                    {/* </div> */}
+                    {/* HONEY POT */}
+                    <div
+                        style={{
+                            position: 'absolute',
+                            left: '-5000px'
+                        }}
+                        aria-hidden="true">
+                        <input
+                            id="form-subscribe__phone"
+                            type="text"
+                            name="b_9000c9c71a9eb1030fd6c4320_191d4d44cb"
+                            tabindex="-1"
+                            value=""
+                        />
+                    </div>
+                    <div className="form-subscribe__button-wrapper">
+                        <button
+                            id="contact-form-submit"
+                            className="fill-in-effect-orange"
+                            type="submit"
+                            onClick={onFormSubmission}
+                        >SUBSCRIBE</button>
+                    </div>
+                </div></> :
+                <>
+                    <div className="form-subscribe__copy">
+                        Thank you for subscribing!
+            </div>
+                </>
+            }
+        </form>
     </div>
 
 };
