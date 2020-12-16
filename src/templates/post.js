92fd1f06dfb2543e3ca4b3a4c02e82b2881524a3
seandolinar <sjjd199@gmail.com>
2020-12-16 15:33:37 -0500

    added two og tags

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react"
-import { graphql } from "gatsby"
-import PropTypes from "prop-types"
+import React, { Component } from "react";
+import { graphql } from "gatsby";
+import PropTypes from "prop-types";
 // // import PostIcons from "../components/PostIcons"
-import Img from "gatsby-image"
+import Img from "gatsby-image";
 import ImageFallback from '../components/ImageFallback';
 import Layout from "../components/layout";
 import BlogSidebar from '../components/blog/BlogSidebar';
@@ -16,64 +16,67 @@ import '../styles/post.scss';
 
 class PostTemplate extends Component {
 
-  componentDidMount() {
+    componentDidMount() {
     // console.log(document.getElementById('post-content').children)
     // document.getElementById('post-content').children
-  }
+    }
 
 
-  render() {
+    render() {
 
-    const post = this.props.data.wordpressPost;
-    const entryDate = new Date(post.date);
+        const post = this.props.data.wordpressPost;
+        const entryDate = new Date(post.date);
 
-    const featuredImage = post.featured_media && post.featured_media.localFile && post.featured_media.localFile.childImageSharp.fluid
+        const featuredImage = post.featured_media && post.featured_media.localFile && post.featured_media.localFile.childImageSharp.fluid;
 
-    return (
-      <Layout
-        headerFontColor="dark"
-        headerSubTitle={post.title}
-      >
-        <Helmet
-          title={`${post.title} - Skinny Tie Media`}
-        />
-        <div className="post-wrapper" >
-          <div className="post-header post-featured-image">
-            {!!featuredImage ? <Img fluid={featuredImage} /> : <ImageFallback />}
-          </div>
-          <div className="post-content" >
-            <div className="post-meta">
-              <div className="post-meta__content">
-                <div className="post-meta__form">
+        return (
+            <Layout
+                headerFontColor="dark"
+                headerSubTitle={post.title}
+            >
+                <Helmet
+                    title={`${post.title} - Skinny Tie Media`}
+                >
+                    <meta property="og:title" content={post.title} />
+                    <meta property="og:image" content={post.featured_media && post.featured_media.source_url} />
+                </Helmet>
+                <div className="post-wrapper" >
+                    <div className="post-header post-featured-image">
+                        {!!featuredImage ? <Img fluid={featuredImage} /> : <ImageFallback />}
+                    </div>
+                    <div className="post-content" >
+                        <div className="post-meta">
+                            <div className="post-meta__content">
+                                <div className="post-meta__form">
+                                </div>
+                                <div className="post-meta-time-stamp">
+                                    <span>📅</span>
+                                    <time className="entry-date" dateTime={post.date}>{entryDate.toLocaleString('en-us', { month: 'long', day: 'numeric', year: 'numeric' })}</time>
+                                </div>
+                                {post.acf.location && <div className="post-meta-location">
+                                    <span>🌎</span>
+                                    <span>{post.acf.location}</span>
+                                </div>}
+                            </div>
+                            {/* <div className="post-meta__spacer"></div> */}
+                        </div>
+                        <div className="post-content-copy" dangerouslySetInnerHTML={{ __html: post.content }} />
+                        <BlogSidebar />
+                    </div>
+                    <FormSubscribe />
                 </div>
-                <div className="post-meta-time-stamp">
-                  <span>📅</span>
-                  <time className="entry-date" dateTime={post.date}>{entryDate.toLocaleString('en-us', { month: 'long', day: 'numeric', year: 'numeric' })}</time>
-                </div>
-                {post.acf.location && <div className="post-meta-location">
-                  <span>🌎</span>
-                  <span>{post.acf.location}</span>
-                </div>}
-              </div>
-              {/* <div className="post-meta__spacer"></div> */}
-            </div>
-            <div className="post-content-copy" dangerouslySetInnerHTML={{ __html: post.content }} />
-            <BlogSidebar />
-          </div>
-          <FormSubscribe />
-        </div>
-        <Footer />
-      </Layout>
-    )
-  }
+                <Footer />
+            </Layout>
+        );
+    }
 }
 
 PostTemplate.propTypes = {
-  data: PropTypes.object.isRequired,
-  edges: PropTypes.array,
-}
+    data: PropTypes.object.isRequired,
+    edges: PropTypes.array,
+};
 
-export default PostTemplate
+export default PostTemplate;
 
 export const pageQuery = graphql`
   query($id: String!) {
@@ -101,6 +104,6 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
 
 
