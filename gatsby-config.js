dc31e0e0156c9a216cf4ad9b8580fa411256d2b5
Nina <ninamusser@gmail.com>
2022-07-13 09:33:58 -0400

    Adding gatsby-plugin-manifest back in

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,18 @@ module.exports = {
                 ]
             }
         },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: 'skinny-tie-media',
+                short_name: 'starter',
+                start_url: '/',
+                background_color: '#663399',
+                theme_color: '#663399',
+                display: 'minimal-ui',
+                icon: 'src/images/logo-tag.png', // This path is relative to the root of the site.  
+            },
+        },
         'gatsby-transformer-sharp',
         'gatsby-plugin-sharp',
         {
