0ce57e804171d4089a7fdf1c14fc10c191f2d950
Nina <ninamusser@gmail.com>
2022-07-13 11:38:43 -0400

    Modify gatsby-source-wordpress config

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,32 +62,11 @@ module.exports = {
                 // routes with the ID option_page_1 and option_page_2
                 // Dashes in IDs will be converted to underscores for use in GraphQL
                 acfOptionPageIds: [],
-                auth: {
-                    // If auth.user and auth.pass are filled, then the source plugin will be allowed
-                    // to access endpoints that are protected with .htaccess.
-                    htaccess_user: "your-htaccess-username",
-                    htaccess_pass: "your-htaccess-password",
-                    htaccess_sendImmediately: false,
-
-                    // If hostingWPCOM is true then you will need to communicate with wordpress.com API
-                    // in order to do that you need to create an app (of type Web) at https://developer.wordpress.com/apps/
-                    // then add your clientId, clientSecret, username, and password here
-                    // wpcom_app_clientSecret:
-                    //   "NMPnXYFtj2gKas7V1kZyMxr7oLry9V5ZxIyBQGu2txjVHg0GhFz6RYcKopkHICYg",
-                    // wpcom_app_clientId: "54793",
-                    // wpcom_user: "gatsbyjswpexample@gmail.com",
-                    // wpcom_pass: "very-secured-password",
-                },
                 // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
                 // It can help you debug specific API Endpoints problems.
                 verboseOutput: false,
                 // Set how many pages are retrieved per API request.
                 perPage: 100,
-                // Search and Replace Urls across WordPress content.
-                searchAndReplaceContentUrls: {
-                    sourceUrl: "https://source-url.com",
-                    replacementUrl: "https://replacement-url.com",
-                },
                 // Set how many simultaneous requests are sent at once.
                 concurrentRequests: 10,
                 // Set WP REST API routes whitelists
@@ -109,7 +88,11 @@ module.exports = {
                     "/*/*/users",
                 ],
                 // Blacklisted routes using glob patterns
-                excludedRoutes: ["/*/*/posts/1456"]
+                excludedRoutes: ["/*/*/posts/1456"],
+                // use a custom normalizer which is applied after the built-in ones.
+                normalizer: function ({ entities }) {
+                    return entities;
+                },
             },
         },
         {
