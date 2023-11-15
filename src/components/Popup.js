b7a8338f0385e42d2cb43ea910906123da28452b
Nina <ninamusser@gmail.com>
2023-11-15 18:42:37 -0500

    Updated popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import '../styles/components/popup.css';
 import $ from 'jquery';
 
+const logo = require('../images/kindling-logo.webp');
+
 const Popup = () => {
   
   
@@ -13,7 +15,7 @@ const Popup = () => {
   return (
     <div className="popupwrapper">
     <div className="overlay"></div>
-    <div className="popup"><h2>SkinnyTie Media is now Kindling Culture Agency.</h2><p>Same great team.<br/>Same amazing work.<br/>Bigger vision.</p><p>Spark something BIG and see what we're up to over at <a href=" https://thekindlingagency.com">thekindlingagency.com</a></p><p>P.S. All Ties Welcome!</p></div>
+    <div className="popup"><h2>SkinnyTie Media is now <img src={logo} alt="Kindling Culture Agency" className="logo" /></h2><p>Same great team.<br/>Same amazing work.<br/>Bigger vision.</p><p>Spark something big and see what we're up to over at <a href=" https://thekindlingagency.com">thekindlingagency.com</a></p></div>
     </div>
   );
 }
